72b169028681c133e7524986e9ee753e13fb31dd
chiruhasbobbadi <chiruhas.bobbadi123@gmail.com>
2022-10-28 16:38:59 -0700

    connecting db

--- nav2.php
+++ nav2.php
@@ -2,15 +2,15 @@
 <nav class="main-nav-outer" id="test">
     <div class="container">
         <ul class="main-nav">
-            <li class="small-logo"><a href="/BurgerShack/index.php"><img class="headerlogo" style="width:30%" src="/BurgerShack/img/logo.png" alt=""></a></li>
-            <li><a href="/BurgerShack/index.php#about">ABOUT</a></li>
-            <li><a href="/BurgerShack/index.php#menu">MENU</a></li>
-            <li><a href="/BurgerShack/index.php#news">NEWS</a></li>
-            <li><a href="/BurgerShack/index.php#devContact">Contact</a></li>
-            <li><a href="/BurgerShack/index.php#login.php">Login</a></li>
+            <li class="small-logo"><a href="/BurgerShack-CMPE-27/index.php"><img class="headerlogo" style="width:30%" src="/BurgerShack-CMPE-27/img/logo.png" alt=""></a></li>
+            <li><a href="/BurgerShack-CMPE-27/index.php#about">ABOUT</a></li>
+            <li><a href="/BurgerShack-CMPE-27/index.php#menu">MENU</a></li>
+            <li><a href="/BurgerShack-CMPE-27/index.php#news">NEWS</a></li>
+            <li><a href="/BurgerShack-CMPE-27/index.php#devContact">Contact</a></li>
+            <li><a href="/BurgerShack-CMPE-27/index.php#login.php">Login</a></li>
 
 
         </ul>
-        <a class="res-nav_click" href="/BurgerShack/index.php"><i class="fa-bars"></i></a>
+        <a class="res-nav_click" href="/BurgerShack-CMPE-272/index.php"><i class="fa-bars"></i></a>
     </div>
 </nav>
