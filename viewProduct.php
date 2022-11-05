10399549d2d5b4d4b8af06f5a9d5cbab7ed5e53a
Chiruhas Bobbadi <40091219+ChiruhasBobbadi@users.noreply.github.com>
2022-11-04 23:20:19 -0700

    Update viewProduct.php

--- viewProduct.php
+++ viewProduct.php
@@ -155,7 +155,21 @@ setcookie($cookie_name,$cookie_val,time()+60*60*24,'/');
 </head>
 <body>
 
-<?php require 'nav2.php'?>
+<nav class="main-nav-outer" id="test">
+    <div class="container">
+        <ul class="main-nav">
+            <li class="small-logo"><a href="/BurgerShack-CMPE-272/index.php"><img class="headerlogo" style="width:30%" src="/BurgerShack-CMPE-272/img/logo.png" alt=""></a></li>
+            <li><a href="/BurgerShack-CMPE-272/index.php#about">ABOUT</a></li>
+            <li><a href="/BurgerShack-CMPE-272/index.php#menu">MENU</a></li>
+            <li><a href="/BurgerShack-CMPE-272/index.php#news">NEWS</a></li>
+            <li><a href="/BurgerShack-CMPE-272/index.php#devContact">Contact</a></li>
+            <li><a href="/BurgerShack-CMPE-272/index.php#login.php">Login</a></li>
+
+
+        </ul>
+        <a class="res-nav_click" href="/BurgerShack-CMPE-272/index.php"><i class="fa-bars"></i></a>
+    </div>
+</nav>
 
 <br>
 <br>
