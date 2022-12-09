49fec697dd4f0a4311c81b823c0af91a4ab82a1b
chiruhasbobbadi <chiruhas.bobbadi123@gmail.com>
2022-12-08 23:52:38 -0800

    curl expose

--- curl_expose_topViewed.php
+++ curl_expose_topViewed.php
@@ -4,9 +4,9 @@
 $sql = "SELECT * FROM products order by count desc limit 5";
 $result = $conn->query($sql);
 $row = $result->fetch_assoc();
-$users = $row["name"]." ".$row["count"];
+$users = $row["name"]." ".$row["count"]." Views";
 while($row = $result->fetch_assoc()){
-    $users = $users.",".$row["name"]." ".$row["count"];
+    $users = $users.",".$row["name"]." ".$row["count"]." Views";
 }
 echo $users;
 ?>
